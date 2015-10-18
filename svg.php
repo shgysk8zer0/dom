0ac1ffcfc1f6121c59f264266b6910aa7860c6f8
Chris Zuber <shgysk8zer0@gmail.com>
2015-10-18 15:36:33 -0700

    Update documentation

--- svg.php
+++ svg.php
@@ -1,5 +1,26 @@
 <?php
-
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\DOM
+ * @version 0.0.1
+ * @since 0.0.1
+ * @copyright 2015, Chris Zuber
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (GPL-2.0)
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 namespace shgysk8zer0\DOM;
 
 use \shgysk8zer0\Core_API as API;
@@ -14,6 +35,12 @@ class SVG extends \DOMDocument implements API\Interfaces\Magic_Methods, API\Inte
 	const CHARSET = 'UTF-8';
 	const CONTENT_TYPE = 'image/svg+xml';
 
+	/**
+	 * Create a new DOMDocument with SVG as the documentElement
+	 *
+	 * @param array  $attrs     Array of attributes to set
+	 * @param string $charset  Character encoding to use
+	 */
 	public function __construct(array $attrs = array(), $charset = self::CHARSET)
 	{
 		parent::__construct('1.0', $charset);
--- traits/documentelementstring.php
+++ traits/documentelementstring.php
@@ -1,9 +1,40 @@
 <?php
-
+/**
+ * @author Chris Zuber <shgysk8zer0@gmail.com>
+ * @package shgysk8zer0\DOM
+ * @subpackage Traits
+ * @subpackage Traits
+ * @version 0.0.1
+ * @since 0.0.1
+ * @copyright 2015, Chris Zuber
+ * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (GPL-2.0)
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
 namespace shgysk8zer0\DOM\Traits;
 
+/**
+ * Trait for DOMDocument to return the contents of documentElement as a string
+ */
 trait DocumentElementString
 {
+	/**
+	 * Returns contents of documentElement as an XML string
+	 *
+	 * @return string XML string from documentElement
+	 */
 	final public function __toString()
 	{
 		return $this->saveXML($this->documentElement);
