4f7af0ac30e05838fea2bc5147abbd9a89c758e7
Chris Zuber <shgysk8zer0@gmail.com>
2017-01-16 17:01:08 -0800

    Allow append do also append children
    - Call method recursively on `$children` array to append multiple
    levels of nodes at once.

--- traits/domelement.php
+++ traits/domelement.php
@@ -5,7 +5,7 @@
  * @subpackage Traits
  * @version 0.0.1
  * @since 0.0.1
- * @copyright 2015, Chris Zuber
+ * @copyright 2017, Chris Zuber
  * @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (GPL-2.0)
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -38,10 +38,24 @@ trait DOMElement
 	 * @param  array  $attributes An optional array of attributes to set
 	 * @return \DOMElement        The newly created and appended node
 	 */
-	final public function append($tag, $content = null, array $attributes = array())
+	final public function append(
+		$tag,
+		$content          = null,
+		array $attributes = array(),
+		array $children   = array()
+	)
 	{
 		$node = $this->appendChild($this->createElement($tag, $content));
-		array_map([$node, 'setAttribute'], array_keys($attributes), array_values($attributes));
+		array_map(
+			[$node, 'setAttribute'],
+			array_keys($attributes),
+			array_values($attributes)
+		);
+
+		foreach ($children as $child) {
+			call_user_func_array([$node, __FUNCTION__], $child);
+		}
+
 		return $node;
 	}
 }
